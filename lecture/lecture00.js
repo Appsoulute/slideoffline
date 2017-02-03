d2e133ac9cf56dab08c82a9fc6495f649ca0cdca
JAE HO LEE <spectrick@gmail.com>
2017-02-03 21:21:10 +0900

    테스트용 title class 오류 수정. 재 변환 및 적용 필요.

--- lecture/lecture00.js
+++ lecture/lecture00.js
@@ -1,7 +1,7 @@
 ﻿lectureArray.push(
 '\n' +
 '<section class="slide-content" title="Slide content" style="height:540px;">\n' +
-'  <div class="" title="" style="height:197px; top:87px; left:41px">\n' +
+'  <div class="title">\n' +
 '    <p style="font-weight:normal;font-style:normal;text-decoration:none;background-color:inherit;color:#000000;font-size:52.0pt;font-variant:normal;text-align:center;direction:ltr;">TOT</p></div>\n' +
 '  <div class="shape" title="" style="height:65px; top:306px; left:41px">\n' +
 '    <p style="font-weight:normal;font-style:normal;text-decoration:none;background-color:inherit;color:#595959;font-size:28.0pt;font-variant:normal;text-align:center;direction:ltr;">GNB</p></div></section>\n' +
